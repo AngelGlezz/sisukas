97649247be1900dc2977ce13170fb518ecfae52b
Angel <angelglezz.7@gmail.com>
2021-01-05 17:21:29 -0600

    test animaciones

--- js/page.js
+++ js/page.js
@@ -23,4 +23,35 @@ $(document).ready(function(){
         $(".principal-section").hide();
         $("#cover").fadeIn("slow");
     });
+
+    $("#eventos").click(function(){
+        $(".principal-section").hide();
+        $("#galery").fadeIn("slow");
+
+        $('.lazy').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 10,
+        }).slick('unslick');
+    
+        setTimeout(function() {
+            $('.lazy').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            })
+        }, 20);
+    });
+
+    $("#text-cover").mouseenter(function(){
+        $("#text-cover").css({"z-index": "3", "background-image": "url(images/conocenos-a.png)"});
+    }).mouseleave(function(){
+        $("#text-cover").css({"z-index": "1", "background-image": "url(images/conocenos.png)"});
+    });
+
+    $("#player").mouseenter(function(){
+        $("#text-cover").css({"z-index": "3", "background-image": "url(images/conocenos-a.png)"});
+    }).mouseleave(function(){
+        $("#text-cover").css({"z-index": "1", "background-image": "url(images/conocenos.png)"});
+    });
 });
