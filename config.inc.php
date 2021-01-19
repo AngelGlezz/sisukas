ec17a1f5224cf2b7912c27038dd1cdebcd3bdd6d
Angel <angelglezz.7@gmail.com>
2021-01-19 17:43:59 -0600

    php pass hide

--- config.inc.php
+++ config.inc.php
@@ -10,8 +10,8 @@ $_nameNoReplay = "Registro Tochito";
 
 $_smtpHost = "smtp.gmail.com";
 $_smtpPort = 587;
-$_smtpUser = "tochito.mailing@gmail.com";
-$_smtpPass = "ibikdiiydbqjloye";
+$_smtpUser = "@gmail.com";
+$_smtpPass = "pass";
 $_smtpAuth = true;
 $_smtpSecure = "tls"; //ssl o tls
 $version = "1.1.1";
